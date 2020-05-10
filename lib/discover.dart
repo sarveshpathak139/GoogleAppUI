b53245690ac2b372ba8cea58b6ff647eb04a0d68
sarveshpathak139 <sarveshpathak139@gmail.com>
2020-05-10 19:59:26 +0530

    Removed SafeArea

--- lib/discover.dart
+++ lib/discover.dart
@@ -9,148 +9,146 @@ class DiscoverPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-            body: ListView(scrollDirection: Axis.vertical, children: <Widget>[
-          Column(children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              
-              children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                              child: CircleAvatar(
-                    radius: 20,
-                    backgroundImage: AssetImage('lib/images/dashflutter.png')),
-              )
-            ]),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              
-              children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Expanded(
-                  child: Container(child: Image(
-                    height: 150,
-                    fit: BoxFit.fitWidth,
-                    image: AssetImage('lib/images/googledoodle.gif'),)))
-              )
-            ]),
+      home: Scaffold(
+          body: ListView(scrollDirection: Axis.vertical, children: <Widget>[
+        Column(children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
 
-            Card(
-              elevation: 2.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0)
-              ),
-              child: ListTile(
-                leading: Icon(Icons.search),
-                title: Text('Search'),
-                trailing: Icon(Icons.mic),
-              )
-            ),
-            SizedBox(height: 5.0,),
-            Card(
-              elevation: 2.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-               
-              ),
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children:<Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Image(
-                            height: 170,
-                            width: 370,
-                            image: AssetImage('lib/images/dashflutter.png'))),
-                            //SizedBox(width: 5.0,),
-                           
-                      ]
-                    ),
-                     Text('Dash Flutter', style: TextStyle(fontSize: 16),),
-                     ListTile(
-                       leading: Icon(Icons.android),
-                        title: Text('Android News'),
-                        trailing: Icon(Icons.more_vert),
-                     )
-                  ],
-                )
-            ),
-            SizedBox(height: 5.0,),
-            Card(
-              elevation: 2.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-               
-              ),
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children:<Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Image(
-                            height: 170,
-                            width: 370,
-                            image: AssetImage('lib/images/dashflutter.png'))),
-                            //SizedBox(width: 5.0,),
-                           
-                      ]
-                    ),
-                     Text('Dash Flutter', style: TextStyle(fontSize: 16),),
-                     ListTile(
-                       leading: Icon(Icons.android),
-                        title: Text('Android News'),
-                        trailing: Icon(Icons.more_vert),
-                     )
-                  ],
-                )
-            ),
-            SizedBox(height: 5.0,),
-            Card(
-              elevation: 2.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-               
-              ),
-               child: Column(
-                 mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children:<Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Image(
-                            height: 170,
-                            width: 370,
-                            image: AssetImage('lib/images/dashflutter.png'))),
-                            //SizedBox(width: 5.0,),
-                           
-                      ]
-                    ),
-                     Text('Dash Flutter', style: TextStyle(fontSize: 16),),
-                     ListTile(
-                       leading: Icon(Icons.android),
-                        title: Text('Android News'),
-                        trailing: Icon(Icons.more_vert),
-                     )
-                  ],
-                )
+            children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+                            child: CircleAvatar(
+                  radius: 20,
+                  backgroundImage: AssetImage('lib/images/dashflutter.png')),
             )
+          ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Expanded(
+                child: Container(child: Image(
+                  height: 150,
+                  fit: BoxFit.fitWidth,
+                  image: AssetImage('lib/images/googledoodle.gif'),)))
+            )
+          ]),
+
+          Card(
+            elevation: 2.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0)
+            ),
+            child: ListTile(
+              leading: Icon(Icons.search),
+              title: Text('Search'),
+              trailing: Icon(Icons.mic),
+            )
+          ),
+          SizedBox(height: 5.0,),
+          Card(
+            elevation: 2.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+
+            ),
+             child: Column(
+               mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:<Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Image(
+                          height: 170,
+                          width: 370,
+                          image: AssetImage('lib/images/dashflutter.png'))),
+                          //SizedBox(width: 5.0,),
+
+                    ]
+                  ),
+                   Text('Dash Flutter', style: TextStyle(fontSize: 16),),
+                   ListTile(
+                     leading: Icon(Icons.android),
+                      title: Text('Android News'),
+                      trailing: Icon(Icons.more_vert),
+                   )
+                ],
+              )
+          ),
+          SizedBox(height: 5.0,),
+          Card(
+            elevation: 2.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+
+            ),
+             child: Column(
+               mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:<Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Image(
+                          height: 170,
+                          width: 370,
+                          image: AssetImage('lib/images/dashflutter.png'))),
+                          //SizedBox(width: 5.0,),
+
+                    ]
+                  ),
+                   Text('Dash Flutter', style: TextStyle(fontSize: 16),),
+                   ListTile(
+                     leading: Icon(Icons.android),
+                      title: Text('Android News'),
+                      trailing: Icon(Icons.more_vert),
+                   )
+                ],
+              )
+          ),
+          SizedBox(height: 5.0,),
+          Card(
+            elevation: 2.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+
+            ),
+             child: Column(
+               mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:<Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Image(
+                          height: 170,
+                          width: 370,
+                          image: AssetImage('lib/images/dashflutter.png'))),
+                          //SizedBox(width: 5.0,),
+
+                    ]
+                  ),
+                   Text('Dash Flutter', style: TextStyle(fontSize: 16),),
+                   ListTile(
+                     leading: Icon(Icons.android),
+                      title: Text('Android News'),
+                      trailing: Icon(Icons.more_vert),
+                   )
+                ],
+              )
+          )
 
 
 
-          ])
-        ])),
-      ),
+        ])
+      ])),
     );
   }
 }
